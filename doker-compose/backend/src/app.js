b9291b46acd87e98ce73d0c81250f2f0dddec7ae
arunz6 <arunrajput88176894@gmail.com>
2026-09-03 16:18:21 +0530

    Add morgan middleware for logging in app.js

--- doker-compose/backend/src/app.js
+++ doker-compose/backend/src/app.js
@@ -1,15 +1,15 @@
 import express from "express";
-
-
+import morgan from "morgan";
 
 const app = express();
+app.use(express.json())
+app.use(morgan("dev"));
 
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "api is working"
+    });
+});
 
-
-
-
-
-
-
-export default  app
+export default app;
